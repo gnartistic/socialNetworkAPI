9e15958b1ea62899cce45de48a587ba2f68179f3
charlie <gn4rtistic@gmail.com>
2022-08-27 12:45:03 -0500

    laid out user-routers

---
+++ routes/api/user-routes.js
@@ -0,0 +1,26 @@
+const router = reuire( 'express' ).Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require( '../../controllers/user-controller' );
+
+router
+    .router( '/' )
+    .get( getAllUsers )
+    .post( createUser );
+
+router.route( '/:id' )
+    .get( getUserById )
+    .put( updateUser )
+    .delete( deleteUser )
+    
+router.route( '/:userId/friends/:friendId' )
+    .post( addFriend )
+    .delete( deleteFriend )
+
+module.exports = router;
